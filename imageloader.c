8998a5a7f08a85e54bbb920f81f80dab43f92e78
Si11ycheese <si11ycheese6680@gmail.com>
2024-03-12 20:47:48 +0800

    commit 1

--- imageloader.c
+++ imageloader.c
@@ -26,16 +26,50 @@
 Image *readData(char *filename) 
 {
 	//YOUR CODE HERE
+    FILE *fp= fopen(filename,"r");
+    if(fp==NULL){
+        return NULL;
+    }
+    Image *image=(Image *) malloc(sizeof(Image));
+    fscanf(fp,"%u",&image->rows);
+    fscanf(fp,"%u",&image->cols);
+    int sum=image->cols*image->rows;
+    for(int i=0;i<sum;i++){
+        *(image->image+i)=(Color *) malloc(sizeof(Color));
+        Color *Pixel=*(image->image+i);
+        fscanf(fp,"%hhu %hhu %hhu",&Pixel->R,&Pixel->G,&Pixel->B);
+    }
+    fclose(fp);
+    return image;
+
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
 	//YOUR CODE HERE
+    printf("P3\n%d %d\n255\n", image->cols, image->rows);
+    Color** p = image->image;
+    for (int i = 0; i < image->rows; i++) {
+        for (int j = 0; j < image->cols - 1; j++) {
+            printf("%3hhu %3hhu %3hhu   ", (*p)->R, (*p)->G, (*p)->B);
+            p++;
+        }
+        printf("%3hhu %3hhu %3hhu\n", (*p)->R, (*p)->G, (*p)->B);
+        p++;
+    }
+
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
 	//YOUR CODE HERE
+    int sum=image->cols*image->rows;
+    for(int i=0;i<sum;i++){
+        free(*(image->image+i));
+    }
+    free(image->image);
+    free(image);
+
 }
